9a5ad498954e1f9e846c525141e658fd17ba2b4c
Gagaro <gagaro42@gmail.com>
2014-04-11 11:09:40 +0200

    fix: unallow anonymous on redirect

--- collective/whathappened/browser/redirect.py
+++ collective/whathappened/browser/redirect.py
@@ -1,3 +1,6 @@
+from AccessControl.unauthorized import Unauthorized
+
+from Products.CMFCore.utils import getToolByName
 from Products.Five.browser import BrowserView
 
 from collective.whathappened.storage_manager import StorageManager
@@ -5,6 +8,11 @@ from collective.whathappened.storage_manager import StorageManager
 
 class RedirectView(BrowserView):
     def __call__(self):
+        mtool = getToolByName(self.context, 'portal_membership')
+        user = mtool.getAuthenticatedMember().getId()
+        if user is None:
+            raise Unauthorized
+
         redirect = self.request.get('redirect', None)
         path = self.request.get('path', None)
         if redirect is None or path is None:
